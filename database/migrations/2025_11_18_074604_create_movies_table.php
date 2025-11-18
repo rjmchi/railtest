7a31a25f43999cfcce9e76d2271e3184109a868e
rjmchi <robert@rjmprogramming.com>
2025-11-18 02:07:32 -0600

    chagnes migrateion

--- database/migrations/2025_11_18_074604_create_movies_table.php
+++ database/migrations/2025_11_18_074604_create_movies_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('movies', function (Blueprint $table) {
+        Schema::create('movies-x', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->integer('rank');
